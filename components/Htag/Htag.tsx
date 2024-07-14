4392539e1172f465d62221d2070d99d91c03ab18
ArtyomIzmaylov <artyomguybov2002@gmail.com>
2024-07-14 14:36:12 +0300

    first commit

---
+++ components/Htag/Htag.tsx
@@ -0,0 +1,5 @@
+
+
+export const Htag = (): JSX.Element => {
+    return (<></>);
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import React from "react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (<>
+    <Head>
+      <title>MyTop !</title>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
+    </Head>
+        sadkasdasjdajjdas
+        <Component {...pageProps} />;
+  </>
+  );
+
 }
